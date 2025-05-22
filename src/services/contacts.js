a831979a0b499e56a71125a7b39203f83808aeb7
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-05-22 16:08:38 +0200

    homework3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,7 +5,32 @@ export const getAllContacts = async () => {
   return contacts;
 };
 
-export const getContactbyId = async (contactId) => {
+export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return contact;
+};
+
+export const patchContact = async (contactId, payload) => {
+  const newContact = await ContactsCollection.findOneAndUpdate(
+    {
+      _id: contactId,
+    },
+    payload,
+    {
+      new: true,
+    },
+  );
+  return newContact;
 };
